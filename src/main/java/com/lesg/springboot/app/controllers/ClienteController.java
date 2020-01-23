6a2ac58a76b83e3d2194db0fb318c707c5662405
alphamen01 <lesg.2018@hotmail.com>
2020-01-23 12:58:34 -0500

    AGREGUE EL ELIMINAR ARCHIVO DE IMAGEN

--- src/main/java/com/lesg/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/lesg/springboot/app/controllers/ClienteController.java
@@ -1,5 +1,6 @@
 package com.lesg.springboot.app.controllers;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
@@ -10,6 +11,7 @@ import java.util.UUID;
 
 import javax.validation.Valid;
 
+//import org.apache.tomcat.util.http.fileupload.UploadContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,10 +48,12 @@ public class ClienteController {
 	private IClienteService clienteService;
 	private final Logger log = LoggerFactory.getLogger(getClass());
 	
+	private  final static String UPLOADS_FOLDER = "uploads";
+	
 	
 	@GetMapping(value="/uploads/{filename:.+}")
 	public ResponseEntity<Resource> verFoto(@PathVariable String filename){
-		Path pathFoto = Paths.get("uploads").resolve(filename).toAbsolutePath();
+		Path pathFoto = Paths.get(UPLOADS_FOLDER).resolve(filename).toAbsolutePath();
 		log.info("pathFoto: " + pathFoto);
 		
 		Resource recurso =null;
@@ -138,14 +142,30 @@ public class ClienteController {
 	
 	if(!foto.isEmpty()) {
 		
+		if(cliente.getId() != null 
+				&& cliente.getId() > 0
+				&& cliente.getFoto() != null
+				&& cliente.getFoto().length() > 0) {
+			
+				Path rootPath = Paths.get(UPLOADS_FOLDER).resolve(cliente.getFoto()).toAbsolutePath();
+				File archivo = rootPath.toFile();
+				
+				if(archivo.exists() && archivo.canRead()) {
+					archivo.delete();
+				}
+		}
 		//Path directorioRecursos = Paths.get("src//main//resources//static/uploads");
 		//String rootPath = directorioRecursos.toFile().getAbsolutePath();
 		
 		
 		//String rootPath ="C://Users//luisi//Documents//servidor//uploads";
 		
+		
+		
+		
+		
 		String uniqueFilename = UUID.randomUUID().toString() + "" + foto.getOriginalFilename();
-		Path rootPath = Paths.get("uploads").resolve(uniqueFilename);
+		Path rootPath = Paths.get(UPLOADS_FOLDER).resolve(uniqueFilename);
 		
 		
 		Path rootAbsolutPath = rootPath.toAbsolutePath();
@@ -185,8 +205,20 @@ public class ClienteController {
 	public String eliminar(@PathVariable(value = "id") Long id, RedirectAttributes flash)
 	{
 		if(id>0){
+			
+			Cliente cliente = clienteService.findOne(id);
+			
 			clienteService.delete(id);
 			flash.addFlashAttribute("success", "Cliente eliminado con exito!");
+			
+			Path rootPath = Paths.get(UPLOADS_FOLDER).resolve(cliente.getFoto()).toAbsolutePath();
+			File archivo = rootPath.toFile();
+			
+			if(archivo.exists() && archivo.canRead()) {
+				if(archivo.delete()) {
+					flash.addFlashAttribute("info", "Foto " + cliente.getFoto() + " eliminado con exito!");
+				}
+			}
 		}
 		
 		return "redirect:/listar";
